d16dbf56d5584440a14bc1e1a15a3824ab23521d
Rafał Florczak <florczak.raf@gmail.com>
2024-12-08 17:37:30 +0100

    07: opt

--- 07/solve_a.py
+++ 07/solve_a.py
@@ -7,30 +7,28 @@ lines = sys.stdin.read().splitlines()
 
 
 def check(result, acc, nums):
-    a = acc + nums[0]
-    b = acc * nums[0]
+    a = acc - nums[-1]
+    b, m = divmod(acc, nums[-1])
 
     if len(nums) == 1:
-        if a == result or b == result:
+        if a == 0 or (b == 0 and not m):
             return result
         else:
             return 0
 
-    if a <= result:
-        ar = check(result, a, nums[1:])
+    if a > 0:
+        ar = check(result, a, nums[:-1])
         if ar == result:
             return result
 
-    if b <= result:
-        br = check(result, b, nums[1:])
-        if br == result:
-            return result
+    if b > 0 and not m:
+        return check(result, b, nums[:-1])
 
     return 0
 
 
 for l in lines:
     nums = [int(x) for x in re.findall(r"\d+", l)]
-    sum += check(nums[0], 0, nums[1:])
+    sum += check(nums[0], nums[0], nums[1:])
 
 print(sum)
--- 07/solve_b.py
+++ 07/solve_b.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env pypy3
+#!/usr/bin/env python3
 import sys
 import re
 
@@ -7,36 +7,35 @@ lines = sys.stdin.read().splitlines()
 
 
 def check(result, acc, nums):
-    a = acc + nums[0]
-    b = acc * nums[0]
-    c = int(str(acc) + str(nums[0]))
+    a = acc - nums[-1]
+    b, m = divmod(acc, nums[-1])
+    s = str(acc)
+    c = int(s.removesuffix(str(nums[-1])) or "0") if s.endswith(str(nums[-1])) else None
 
     if len(nums) == 1:
-        if a == result or b == result or c == result:
+        if a == 0 or (b == 0 and not m) or c == 0:
             return result
         else:
             return 0
 
-    if a <= result:
-        ar = check(result, a, nums[1:])
+    if a > 0:
+        ar = check(result, a, nums[:-1])
         if ar == result:
             return result
 
-    if b <= result:
-        br = check(result, b, nums[1:])
+    if b > 0 and not m:
+        br = check(result, b, nums[:-1])
         if br == result:
             return result
 
-    if c <= result:
-        cr = check(result, c, nums[1:])
-        if cr == result:
-            return result
+    if c is not None and c > 0:
+        return check(result, c, nums[:-1])
 
     return 0
 
 
 for l in lines:
     nums = [int(x) for x in re.findall(r"\d+", l)]
-    sum += check(nums[0], 0, nums[1:])
+    sum += check(nums[0], nums[0], nums[1:])
 
 print(sum)
