2cf6b93dd186220a04888dd86972d6bba18e03bd
Rafał Florczak <florczak.raf@gmail.com>
2024-12-09 23:10:48 +0100

    09: simplified b?
    It fails on the test input but seems to work on my data

--- 09/solve_b.py
+++ 09/solve_b.py
@@ -28,44 +28,20 @@ for cur, length, file_id in files[::-1]:
         drive[cur:cur + length] = [file_id] * length
         continue
 
-    used_slot = None
     for i, (slot_pos, slot_length) in enumerate(free_slots):
         if slot_pos > cur:
             drive[cur:cur + length] = [file_id] * length
             break
 
         if slot_length >= length:
-            used_slot = i
+            remaining = slot_length - length
+            if remaining > 0:
+                free_slots[i] = (slot_pos + length, remaining)
+            else:
+                free_slots.pop(i)
+
             drive[slot_pos:slot_pos + length] = [file_id] * length
             break
-    else:
-        drive[cur:cur + length] = [file_id] * length
-
-    if used_slot is not None:
-        remaining = slot_length - length
-        if remaining > 0:
-            free_slots[i] = (slot_pos + length, remaining)
-        else:
-            free_slots.pop(i)
-
-        if not free_slots:
-            free_slots.append((cur, length))
-            continue
-
-        for i, (slot_pos, slot_length) in enumerate(free_slots[:-1]):
-            if slot_pos + slot_length == cur:
-                free_slots[i] = (slot_pos, slot_length + length)
-                if i + 1 < len(free_slots) and cur + length == free_slots[i + 1][0]:
-                    free_slots[i] = (slot_pos, slot_length + length + free_slots[i + 1][1])
-                    free_slots.pop(i + 1)
-                break
-            if i + 1 < len(free_slots) and cur + length == free_slots[i + 1][0]:
-                free_slots[i + 1] = (cur, cur + length + free_slots[i + 1][1])
-                free_slots.pop(i + 1)
-                break
-        else:
-            free_slots.append((cur, length))
-            free_slots.sort()
 
 sum = 0
 
