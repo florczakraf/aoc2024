044dc212790c20378dc05c531421a1200db2083b
Rafał Florczak <florczak.raf@gmail.com>
2024-12-09 22:46:45 +0100

    09: simplify a

--- 09/solve_a.py
+++ 09/solve_a.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
 import sys
-import heapq
 
 dense_line = sys.stdin.read().strip()
 sparse_line = []
 is_file = True
 file_id = 0
-free_slots = []
-cur = 0
+target_pos = 0
 
 for c in dense_line:
     length = int(c)
@@ -16,29 +14,24 @@ for c in dense_line:
         file_id += 1
     else:
         sparse_line.extend([None] * length)
-        free_slots.extend(list(range(cur, cur + length)))
 
     is_file = not is_file
-    cur += length
 
-heapq.heapify(free_slots)
-
-for i in range(len(sparse_line)):
-    if not free_slots:
-        break
-
-    source_pos = len(sparse_line) - i - 1
-    target_pos = heapq.heappop(free_slots)
+for source_pos in range(len(sparse_line) - 1, 0, -1):
     file_id = sparse_line[source_pos]
     if file_id is None:
-        heapq.heappush(free_slots, target_pos)
         continue
 
-    if target_pos > source_pos:
+    for i in range(target_pos, source_pos):
+        if sparse_line[i] is None:
+            target_pos = i
+            break
+    else:
         break
+
     sparse_line[target_pos] = file_id
     sparse_line[source_pos] = None
-    heapq.heappush(free_slots, source_pos)
+    target_pos += 1
 
 sum = 0
 for i, file_id in enumerate(sparse_line):
